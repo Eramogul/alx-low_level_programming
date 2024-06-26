79260b78f892e954171e3cc5e70aff88ddf0f55d
eramogul <dommyemm@yahoo.com>
2024-06-26 13:31:42 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - Prints the alphabet in lowercase 10 times followed by a new line
+ * main - Prints the alphabet x10 in lower case
  */
 void print_alphabet_x10(void)
 {
