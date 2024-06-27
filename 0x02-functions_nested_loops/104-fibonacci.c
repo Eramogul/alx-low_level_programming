a4f31862064f0bef5d43aec250bef07267f141fb
eramogul <dommyemm@yahoo.com>
2024-06-26 21:17:16 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,41 +7,40 @@
  */
 int main(void)
 {
-	unsigned long fib1 = 1, fib2 = 2;
-	unsigned long fib1_half1, fib1_half2, fib2_half1, fib2_half2;
-	unsigned long half1, half2;
-	int count;
+	unsigned long int a = 1, b = 2, next;
+	unsigned long int a_high, a_low, b_high, b_low, next_high, next_low;
+	int i;
 
-	printf("%lu, %lu", fib1, fib2);
+	printf("%lu, %lu", a, b);
 
-	for (count = 2; count < 98; count++)
+	for (i = 2; i < 98; i++)
 	{
-	if (fib1 + fib2 < fib1)
+	if (a + b < a)
 	{
-	fib1_half1 = fib1 / 10000000000;
-	fib1_half2 = fib1 % 10000000000;
-	fib2_half1 = fib2 / 10000000000;
-	fib2_half2 = fib2 % 10000000000;
+	a_high = a / 1000000000;
+	a_low = a % 1000000000;
+	b_high = b / 1000000000;
+	b_low = b % 1000000000;
 
-	half1 = fib1_half1 + fib2_half1;
-	half2 = fib1_half2 + fib2_half2;
-	if (half2 >= 10000000000)
-	{
-	half1++;
-	half2 %= 10000000000;
-	}
+	next_low = a_low + b_low;
+	next_high = a_high + b_high + (next_low / 1000000000);
+	next_low = next_low % 1000000000;
+	printf(", %lu%09lu", next_high, next_low);
 
-	fib1 = fib2;
-	fib2 = half1 * 10000000000 + half2;
+	a_high = b_high;
+	a_low = b_low;
+	b_high = next_high;
+	b_low = next_low;
 
-	printf(", %lu%010lu", half1, half2);
+	a = a_high * 1000000000 + a_low;
+	b = b_high * 1000000000 + b_low;
 	}
 	else
 	{
-	fib1 = fib1 + fib2;
-	fib2 = fib1 - fib2;
-
-	printf(", %lu", fib1);
+	next = a + b;
+	printf(", %lu", next);
+	a = b;
+	b = next;
 	}
 	}
 
