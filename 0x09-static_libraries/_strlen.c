53ebf67054fceb6e71d67b66f3180073831dd68f
eramogul <dommyemm@yahoo.com>
2024-07-17 20:58:22 -0700

    .strlen.c

--- 0x09-static_libraries/_strlen.c
+++ 0x09-static_libraries/_strlen.c
@@ -2,17 +2,18 @@
 
 /**
  * _strlen - returns the length of a string
- * @s: The string to measure
+ * @s  The string to measure
  *
  * Return: The length of the string
  */
+
 int _strlen(char *s)
 {
-    int len = 0;
+	int len = 0;
 
     while (*s++)
     {
-        len++;
+	    len++;
     }
     return (len);
 }
